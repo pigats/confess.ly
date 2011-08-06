95c07091cac78624895cdb96b7f866cdcd229218
pigats <andrea@fruktarbo.com>
2011-08-06 11:35:07 -0700

    skel

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Confessionbox::Application.config.secret_token = 'dca2afee01970259846fcda813506360531e5e7c2517e2dbdf4f70ff0a06707bd46d10e7f5d87dc354444582c58e56bcfcde108c4839adbffeb057b357e0129e'
