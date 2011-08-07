c353a7cd9568c2d6d209f2d8cf94d266af040de5
pigats <andrea@fruktarbo.com>
2011-08-07 15:36:12 -0700

    sass fix for heroku

--- config/compass.rb
+++ config/compass.rb
@@ -1,10 +1,4 @@
 project_type = :rails
-project_path = Compass::AppIntegration::Rails.root
-
-http_path = "/"
-
-environment = Compass::AppIntegration::Rails.env
-if environment == 'production'
-  css_dir = "tmp/stylesheets"
-  sass_dir = "app/assets/stylesheets"
-end
+http_path    = '/'
+css_dir      = 'tmp/stylesheets'
+sass_dir     = 'app/assets/stylesheets'
