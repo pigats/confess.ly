26cbb5aed3d5c1e369b58fa40f5e78d14d7e44c0
pigats <andrea@fruktarbo.com>
2011-08-07 15:15:23 -0700

    compass for heroku

--- config/compass.rb
+++ config/compass.rb
@@ -6,8 +6,5 @@ http_path = "/"
 environment = Compass::AppIntegration::Rails.env
 if environment == 'production'
   css_dir = "tmp/stylesheets"
-  sass_dir = "app/views/stylesheets"
-else
-  css_dir = "public/stylesheets"
-  sass_dir = "app/stylesheets"
+  sass_dir = "app/assets/stylesheets"
 end
