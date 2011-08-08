880f6eac948e4651b33385cf08ebba6df4ede839
pigats <andrea@fruktarbo.com>
2011-08-07 18:33:49 -0700

    nickname

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable #, :confirmable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :nickname, :email, :password, :password_confirmation, :remember_me
 	
 	#validates_uniqueness_of :nickname
 	
